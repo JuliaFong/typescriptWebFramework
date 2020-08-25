398be8bc9fccb3cf150a2c7abfd9aa1844a2358d
Julia Fong <Juliamzfong@gmail.com>
2020-08-25 13:47:23 -0700

    Add save user data in user.ts file

--- dist/src.f10117fe.js
+++ dist/src.f10117fe.js
@@ -1907,7 +1907,7 @@ module.exports.default = axios;
 
 },{"./utils":"node_modules/axios/lib/utils.js","./helpers/bind":"node_modules/axios/lib/helpers/bind.js","./core/Axios":"node_modules/axios/lib/core/Axios.js","./core/mergeConfig":"node_modules/axios/lib/core/mergeConfig.js","./defaults":"node_modules/axios/lib/defaults.js","./cancel/Cancel":"node_modules/axios/lib/cancel/Cancel.js","./cancel/CancelToken":"node_modules/axios/lib/cancel/CancelToken.js","./cancel/isCancel":"node_modules/axios/lib/cancel/isCancel.js","./helpers/spread":"node_modules/axios/lib/helpers/spread.js"}],"node_modules/axios/index.js":[function(require,module,exports) {
 module.exports = require('./lib/axios');
-},{"./lib/axios":"node_modules/axios/lib/axios.js"}],"src/index.ts":[function(require,module,exports) {
+},{"./lib/axios":"node_modules/axios/lib/axios.js"}],"src/models/User.ts":[function(require,module,exports) {
 "use strict";
 
 var __importDefault = this && this.__importDefault || function (mod) {
@@ -1919,12 +1919,86 @@ var __importDefault = this && this.__importDefault || function (mod) {
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+exports.User = void 0;
 
 var axios_1 = __importDefault(require("axios"));
 
-axios_1.default.get('http://localhost:3000/users/1'); //npm run start:db
+var User =
+/** @class */
+function () {
+  function User(data) {
+    this.data = data;
+    this.events = {};
+  }
+
+  User.prototype.get = function (propName) {
+    return this.data[propName];
+  };
+
+  User.prototype.set = function (update) {
+    Object.assign(this.data, update);
+  };
+
+  User.prototype.on = function (eventName, callback) {
+    this.events[eventName]; //Callback[] or undefined
+
+    var handlers = this.events[eventName] || [];
+    handlers.push(callback);
+    this.events[eventName] = handlers;
+  };
+
+  User.prototype.trigger = function (eventName) {
+    var handlers = this.events[eventName];
+
+    if (!handlers || handlers.length === 0) {
+      return;
+    }
+
+    handlers.forEach(function (callback) {
+      callback();
+    });
+  };
+
+  User.prototype.fetch = function () {
+    var _this = this;
+
+    axios_1.default.get("http://localhost:3000/users/" + this.get('id')).then(function (response) {
+      _this.set(response.data);
+    });
+  };
+
+  User.prototype.save = function () {
+    var id = this.get('id');
+
+    if (id) {
+      //put
+      axios_1.default.put("http://localhost:3000/users/" + id, this.data);
+    } else {
+      //post
+      axios_1.default.post('http://localhost:3000/users', this.data);
+    }
+  };
+
+  return User;
+}();
+
+exports.User = User;
+},{"axios":"node_modules/axios/index.js"}],"src/index.ts":[function(require,module,exports) {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var User_1 = require("./models/User");
+
+var user = new User_1.User({
+  name: 'new record',
+  age: 0
+});
+user.save(); //npm run start:db
 //npm run start:parcel
-},{"axios":"node_modules/axios/index.js"}],"../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./models/User":"src/models/User.ts"}],"../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -1952,7 +2026,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61682" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51467" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,9 +1,9 @@
 import axios, { AxiosResponse } from 'axios';
 
 interface UserProps {
-    id?: number;
     name?: string;
     age?: number;
+    id?: number;
 }
 
 type Callback = () => void;
@@ -41,9 +41,20 @@ export class User {
     }
 
     fetch(): void {
-        axios.get(`https://localhost:3000/users/${this.get('id')}`)
-        .then((response: AxiosResponse): void => {
+        axios.get(`http://localhost:3000/users/${this.get('id')}`).then(
+            (response: AxiosResponse): void => {
             this.set(response.data);
         });
+    }
+
+    save(): void {
+        const id = this.get('id');
+       if (id) {
+           //put
+           axios.put(`http://localhost:3000/users/${id}`, this.data)
+       } else {
+           //post
+           axios.post('http://localhost:3000/users', this.data);
+       }
     }
 }
