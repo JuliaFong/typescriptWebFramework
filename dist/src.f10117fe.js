9d4730ee910cefaaf1c3f4e093571e4a1fe05e29
Julia Fong <Juliamzfong@gmail.com>
2020-08-25 11:24:59 -0700

    Update user file logic

--- dist/src.f10117fe.js
+++ dist/src.f10117fe.js
@@ -136,6 +136,12 @@ function () {
     return this.data[propName];
   };
 
+  User.prototype.set = function (update) {
+    Object.assign(this.data, update);
+  };
+
+  User.prototype.on = function (eventName, callback) {};
+
   return User;
 }();
 
@@ -152,6 +158,10 @@ var User_1 = require("./models/User");
 var user = new User_1.User({
   name: "yourname",
   age: 20
+});
+user.set({
+  name: 'newname',
+  age: 666
 });
 console.log(user.get('name'));
 console.log(user.get('age'));
@@ -183,7 +193,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59263" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62145" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,8 @@ import { User } from './models/User'
 
 const user = new User({name: "yourname", age:20})
 
+user.set({ name: 'newname', age: 666 })
+
+
 console.log(user.get('name'))
 console.log(user.get('age'))
