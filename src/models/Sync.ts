6d04085269063a53c61699298d35b23b3e02ad67
Julia Fong <Juliamzfong@gmail.com>
2020-08-25 19:40:21 -0700

    Refactor sync file

--- src/models/Sync.ts
+++ src/models/Sync.ts
@@ -1,6 +1,10 @@
 import axios, { AxiosResponse } from 'axios';
+import { UserProps } from './User';
 
+new Sync('http://localhost:3000/users');
 export class Sync {
+    constructor(public rootUrl: string) {}
+
     fetch(): void {
         axios.get(`http://localhost:3000/users/${this.get('id')}`).then(
             (response: AxiosResponse): void => {
