b84d0887ca47f4fb5697c634718b04a8b8e83db0
Julia Fong <Juliamzfong@gmail.com>
2020-08-31 11:21:02 -0700

    Add views folder

--- dist/src.f10117fe.js
+++ dist/src.f10117fe.js
@@ -2251,7 +2251,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64975" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49406" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
