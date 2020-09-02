cf89726411d119d6cb280d61b98d867eba147abb
Julia Fong <Juliamzfong@gmail.com>
2020-09-01 23:23:00 -0700

    Add view file and refactor user form logic

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,17 +1,6 @@
-import { User } from "../models/User";
+import { View } from './View'
 
-export class UserForm {
-    constructor(
-        public parent: Element,
-        public model: User
-        ) { this.bindModel() } 
-
-        bindModel() : void {
-            this.model.on('change', () => {
-                this.render();
-            });
-        }
-
+export class UserForm  extends View{
     eventsMap(): { [key: string]: () => void } {
         return {
             'click:.set-name': this.onSetNameClick,
@@ -47,29 +36,4 @@ export class UserForm {
         `;
     }
 
-    bindEvents(fragment: DocumentFragment): void {
-        const eventsMap = this.eventsMap();
-
-        for(let eventKey in eventsMap) {
-            //eventName 'click'
-            //selector 'button'
-            const [eventName, selector] = eventKey.split(':');
-
-            fragment.querySelectorAll('.set-age').forEach(
-                element => {
-                    element.addEventListener(eventName, eventsMap[eventKey])
-                });
-            }
-        }
-
-    render(): void {
-        this.parent.innerHTML = '';
-
-        const templateElement = document.createElement('template');
-        templateElement.innerHTML = this.template();
-
-        this.bindEvents(templateElement.content)
-
-        this.parent.append(templateElement.content);
-    }
 }
