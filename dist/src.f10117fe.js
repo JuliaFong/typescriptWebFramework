2716dc092b93675882127bf58016074f29f8a00f
Julia Fong <Juliamzfong@gmail.com>
2020-09-03 12:37:46 -0700

    Add collection view file

--- dist/src.f10117fe.js
+++ dist/src.f10117fe.js
@@ -2559,7 +2559,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63451" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53333" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
